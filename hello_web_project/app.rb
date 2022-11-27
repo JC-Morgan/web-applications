15b1e4b3a37a241e6d6004b945c203ffcb72b9f3
JC Morgan <jcmorgan@mastudemaxzq6l4.home>
2022-11-27 22:37:54 +0000

    Updates to hello_web_project application

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -2,13 +2,21 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Application < Sinatra::Base
+
   configure :development do
     register Sinatra::Reloader
   end
-end
 
-class Application
-  get '/' do
-    "Hello World"
+
+  get '/hello' do
+    name = params[:name]
+    return "Hello #{name}"
   end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    return "Thanks #{name}, you sent this message: #{message}"
+  end
+
 end
